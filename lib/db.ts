801024c76c1c9ecccef7290c801cedebc236ab77
f2firdaus <firdausnaseem@gmail.com>
2025-06-23 12:13:13 +0530

    auth

--- lib/db.ts
+++ lib/db.ts
@@ -5,29 +5,52 @@ const MONGODB_URI = process.env.MONGODB_URI!;
 if (!MONGODB_URI) {
   throw new Error("Please define mongodb uri variables");
 }
-
 let cached = global.mongoose;
+
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 export async function connectToDatabase() {
   if (cached.conn) {
+    
     return cached.conn;
   }
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: true,
-      maxPoolSize: 10,
-    };
-
-    mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
   }
   try {
     cached.conn = await cached.promise;
   } catch (error) {
     cached.promise = null;
-    throw error;
+    return error;
   }
-
   return cached.conn;
 }
+
+// let cached = global.mongoose;
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+// export async function connectToDatabase() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: true,
+//       maxPoolSize: 10,
+//     };
+
+//     mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+
+//     // mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
+//   }
+//   try {
+//     cached.conn = await cached.promise;
+//   } catch (error) {
+//     cached.promise = null;
+//     throw error;
+//   }
+
+//   return cached.conn;
+// }
