8632daf4fdeeda474f8d261a3cc60dfbddb0458e
f2firdaus <firdausnaseem@gmail.com>
2025-06-25 12:26:30 +0530

    added:upload

---
+++ lib/api-client.ts
@@ -0,0 +1,36 @@
+import { IVideo } from "@/app/models/Video";
+
+export type VideoFormData=Omit<IVideo,"_id">
+type FetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: any
+  headers: Record<string, string>
+};
+class APiClient {
+    private async fetch<T>(endpoint: string,
+        options: FetchOptions = {}): Promise<T> {
+    const { method = "GET", body, headers = {} } = options;
+    const defaultHeaders = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
+    const response = await fetch(`/api${endpoint}`, {
+      method,
+      headers: defaultHeaders,
+      body: body ? JSON.stringify(body) : undefined,
+    });
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+    return response.json();
+    }
+    async getVideos() {
+        return this.fetch("/videos")
+    }
+    async createVideo(videoData:VideoFormData) {
+        return this.fetch("/videos", {
+            method: "POST",
+            body:videoData
+        })
+    }
+}
